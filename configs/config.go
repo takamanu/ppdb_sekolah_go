6b7d235cebb1ebd5b4276dbe2b6185b5cbb9cbe1
Takamanu <fauzanalivijsma@gmail.com>
2023-10-29 00:58:55 +0700

    feat: Edit configs for db

--- configs/config.go
+++ configs/config.go
@@ -1,13 +1,15 @@
 package configs
 
 import (
+	"ppdb_sekolah_go/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() {
 
-	dsn := "root:@tcp(127.0.0.1:3306)/my_fashion?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(127.0.0.1:3306)/ppdb_smp?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -15,5 +17,7 @@ func InitDB() {
 	}
 
 	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.Nilai{})
+	DB.AutoMigrate(&models.Datapokok{})
 
 }
