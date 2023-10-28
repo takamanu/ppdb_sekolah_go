d085fe4c2bec59b108f564d0b25da5a15caebde8
Takamanu <fauzanalivijsma@gmail.com>
2023-10-29 00:56:53 +0700

    feat: Edit some datamodel for Users

--- models/Users.go
+++ models/Users.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name" form:"name"`
-	Email    string `json:"email"  form:"email"`
-	Password string `json:"password" form:"password"`
-	Role     uint   `json:"role"  form:"role"`
+	Name      string      `json:"name" form:"name"`
+	Email     string      `json:"email"  form:"email"`
+	Password  string      `json:"password" form:"password"`
+	Role      uint        `json:"role"  form:"role"`
+	Datapokok []Datapokok `json:"datapokok"  form:"datapokok"`
 }
 
 type UserResponse struct {
