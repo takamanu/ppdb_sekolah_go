898ba3262fddb0198318a0491d05b97b49a9fe62
Takamanu <fauzanalivijsma@gmail.com>
2023-10-31 23:18:54 +0700

    fix: add validation for UserController

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -6,6 +6,7 @@ import (
 	"ppdb_sekolah_go/constans"
 	m "ppdb_sekolah_go/middlewares"
 	"ppdb_sekolah_go/models"
+	"regexp"
 
 	loger "log"
 	"net/http"
@@ -14,6 +15,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 func GetUsersController(c echo.Context) error {
@@ -55,6 +57,21 @@ func CreateUserController(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
+	// Validate all required fields
+	if user.Email == "" || user.Password == "" || user.Name == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "All fields except role must be filled")
+	}
+
+	// Validate the email address
+	if !validateEmail(user.Email) {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid email")
+	}
+
+	// Validate the password
+	if len(user.Password) < 8 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Password must be at least 8 characters long")
+	}
+
 	if IsEmailRegistered(user.Email) {
 		return echo.NewHTTPError(http.StatusBadRequest, "Email address is already registered")
 	}
@@ -93,6 +110,62 @@ func DeleteUserController(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusNotFound, "User not found")
 	}
 
+	// Check if the datapokok exists
+	var datapokok models.Datapokok
+	if err := configs.DB.Where("user_id = ?", user.ID).First(&datapokok).Error; err != nil {
+		// If the datapokok doesn't exist, just delete the user
+		if err == gorm.ErrRecordNotFound {
+			if err := configs.DB.Delete(&user).Error; err != nil {
+				log.Errorf("Failed to delete user with id %d: %v", id, err)
+				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to delete user")
+			}
+
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				constans.SUCCESS: true,
+				constans.MESSAGE: "Success deleted user",
+			})
+		}
+
+		// If there's another error, return it
+		return err
+	}
+
+	// Check if the nilai exists
+	var nilai models.Nilai
+	if err := configs.DB.Where("datapokok_id = ?", datapokok.ID).First(&nilai).Error; err != nil {
+		// If the nilai doesn't exist, delete the datapokok and user
+		if err == gorm.ErrRecordNotFound {
+			if err := configs.DB.Delete(&datapokok).Error; err != nil {
+				log.Errorf("Failed to delete datapokok user with id %d: %v", id, err)
+				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to delete datapokok user")
+			}
+
+			if err := configs.DB.Delete(&user).Error; err != nil {
+				log.Errorf("Failed to delete user with id %d: %v", id, err)
+				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to delete user")
+			}
+
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				constans.SUCCESS: true,
+				constans.MESSAGE: "Success deleted user and datapokok",
+			})
+		}
+
+		// If there's another error, return it
+		return err
+	}
+
+	// If all three exist, delete all three
+	if err := configs.DB.Delete(&nilai).Error; err != nil {
+		log.Errorf("Failed to delete nilai user with id %d: %v", id, err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to delete nilai user")
+	}
+
+	if err := configs.DB.Delete(&datapokok).Error; err != nil {
+		log.Errorf("Failed to delete datapokok user with id %d: %v", id, err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to delete datapokok user")
+	}
+
 	if err := configs.DB.Delete(&user).Error; err != nil {
 		log.Errorf("Failed to delete user with id %d: %v", id, err)
 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to delete user")
@@ -100,7 +173,7 @@ func DeleteUserController(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		constans.SUCCESS: true,
-		constans.MESSAGE: "Success deleted user",
+		constans.MESSAGE: "Success deleted user, datapokok, and nilai",
 	})
 }
 
@@ -118,13 +191,28 @@ func UpdateUserController(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "User not found")
 	}
 
+	// Validate email
+	email := c.FormValue("email")
+	if email != "" && !validateEmail(email) {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid email")
+	}
+
+	// Validate password
+	newPassword := c.FormValue("password")
+	if newPassword != "" && len(newPassword) < 8 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Password must be at least 8 characters long")
+	}
+
+	if IsEmailRegistered(email) {
+		return echo.NewHTTPError(http.StatusBadRequest, "Email address is already registered")
+	}
+
 	// bind request body to user struct
 	if err := c.Bind(&user); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
 	// update password if new password is provided
-	newPassword := c.FormValue("password")
 	if newPassword != "" {
 		// encrypt new password
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
@@ -193,4 +281,9 @@ func IsEmailRegistered(email string) bool {
 		return false
 	}
 	return true
+}
+
+func validateEmail(email string) bool {
+	re := regexp.MustCompile(`^[\w-\.]+@([\w-]+\.)+[\w-]{2,}$`)
+	return re.MatchString(email)
 }
