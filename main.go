9f0b3aa9a565d208003752505bc71293a3202fec
Takamanu <fauzanalivijsma@gmail.com>
2023-11-01 06:13:08 +0700

    coba deploy

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"ppdb_sekolah_go/configs"
 	"ppdb_sekolah_go/routes"
 )
@@ -9,7 +10,12 @@ func main() {
 	configs.InitDB()
 	configs.InitGCB()
 
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8000" // Default port if not specified in the environment variable
+	}
+
 	e := routes.New()
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
